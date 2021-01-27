6388caab08dc31de034037306668307d7a846233
Everton Ferreira <ferreira.everton@icloud.com>
2021-01-27 13:06:33 -0300

    Nunjucks

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 
-routes.get('/', (req, res) => res.render('index'));
+routes.get('/', (req, res) => res.render('index.html'));
 
 routes.use((req,res) => res.status(404).render('404'))
 
